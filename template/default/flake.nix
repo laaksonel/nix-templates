40411e4f2fc5f804dc7f066bcd47f0d9e4e4959b
Lauri Laaksonen <laaksonel@gmail.com>
2024-12-01 14:14:37 +0200

    Default template

---
+++ template/default/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = rec {
+        hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        default = hello;
+      };
+    });
+}
